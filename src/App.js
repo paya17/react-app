45e774f582d3035b64605cca4ff487564c384216
ujiniverse <88028826+ujiniverse@users.noreply.github.com>
2021-12-19 01:46:10 +0900

    TOC-component event

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ class App extends Component { //'App컴포넌트'
     super(props)
     this.state = {  
       subject:{title:"WEB" , sub:"world wide web!"}, 
-      mode:'read', //mode프로퍼티 추가
-      welcome:{ title:'Welcome', desc:"Hello, React!!"}, //welcome프로퍼티 추가
+      mode:'read', //*mode프로퍼티 추가
+      selected_content_id:2, //*selected_content_id프로퍼티 추가
+      welcome:{ title:'Welcome', desc:"Hello, React!!"}, 
       contents:[
         {id:1, title:'HTML', desc:"HTML is for information"},
         {id:2, title:'CSS', desc:"CSS is for design"},
@@ -23,24 +24,36 @@ class App extends Component { //'App컴포넌트'
 
   render() {
     var _title, _desc = null; //기본값이 null
-    if(this.state.mode === 'welcome') { //mode에 따라서, Content컴포넌트 내용이 달라지도록
+    if(this.state.mode === 'welcome') { //mode에 따라서, 'Content컴포넌트 내용이 달라지도록'
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc; //나중에 개선할 것임
+      var i = 0;
+      while(i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break; //while문 빠져나감
+        }
+        i = i+1;
+      } //while문 사용
     }
 
     return (
       <div className="App"> 
         <Subject 
-          title={this.state.subject.title} 
+          title={this.state.subject.title}
           sub={this.state.subject.sub}
-          onChangePage={fuction() { {/*onChangePage이벤트(*props->Subject컴포넌트에 전달됨) 만들기*/}
-            this.setState({mode:'welcome'}); {/*mode를 welcome으로 'state 변경'!*/}
+          onChangePage={fuction() {
+            this.setState({mode:'welcome'}); 
           }.bind(this)}
-        ></Subject> 
-        <TOC data={this.state.contents}></TOC>
+          ></Subject> 
+        <TOC 
+          data={this.state.contents}
+          onChangePage={function(id) { {/* *인자로 id값 전달받음*/}
+            this.setState({mode:'read', selected_content_id:Number(id)}); {/* **mode를 read로, selected_content_id를 전달받은 id값으로 'state 변경'!*/}
+          }.bind(this)}></TOC>
         <Content title={_title} desc={_desc}></Content> 
       </div>
     );
@@ -51,32 +64,23 @@ class App extends Component { //'App컴포넌트'
 export default App;
 
 
-//** 1.App컴포넌트에서 내부정보를 state값에 설정 -> 2.state값을 하위 컴포넌트의 props값으로 전달->하위 컴포넌트의 props값이 하위 컴포넌트 파일로 전달됨
-//*App컴포넌트 입장에서는 'state'라는 내부정보 사용했고, 그것을 하위 컴포넌트로 전달할 때는 'props' 이용
 
-//여기부터
-//state,props,'event'가 서로 상호작용하면서, 애플리케이션의 '역동성'을 만든다
 
 //Subject컴포넌트의 WEB이나 TOC컴포넌트의 목록을 '클릭'(event)했을 때->각 부분에 맞게 Content컴포넌트의 내용이 달라지도록 (**App컴포넌트의 'state'를 바꾸는 것을 통해)
 
-//*state값 바뀌면->그 state를 가지고 있는 App컴포넌트의 'render'함수가 다시 호출됨->하위컴포넌트들의 'render'함수도 함께 호출됨 (화면이 다시 그려진다)
 //*(event발생) -> App컴포넌트에서, state 바뀜->하위컴포넌트들의 props 바뀜 -> 하위컴포넌트에서, 바뀐 props (위와 비슷한 얘기)(화면이 다시 그려진다)
-//render함수는 '어떤 HTML을 그릴 것인가'라는 것을 결정하는 함수
-//state,props가 바뀌면, 화면이 다시 그려진다
-
-//리액트에서는, 이벤트 다음에 오는 내용을 문자열("")(js문법)이 아닌, 중괄호({})로 묶기
-//리액트에서는, 이벤트가 발생하면, 함수의 첫번째 매개변수로 '이벤트객체' e를 준다
 
 //**'event발생하면->state값 바뀌도록'해서 역동성(화면이 다시 그려짐) 부여!
 //*state값 변경하려면-> 1.함수 뒤에 '.bind(this)' 추가(강제로 this값 주입) 2.this.setState() 사용(constructor에서는 필요없음)
 
-//이벤트 '만들기'
 //*Subject컴포넌트에서 WEB을 클릭했을 때, 'onClick이벤트'가 발생해서 App컴포넌트에 있는 'onChangePage이벤트'의 function이 호출됨->mode를 welcome으로 바꾸는 'state값 변경'이 일어남->Content컴포넌트의 내용이 바뀜
 //책 p111 참고!
 
+//여기부터
+//*TOC컴포넌트에서 목록 중 하나를 클릭했을 때, 'onClick이벤트'가 발생해서 App컴포넌트에 있는 'onChangePage이벤트'의 funtion이 호출됨->*mode를 read로, selected_content_id를 전달받은 id값으로 바꾸는 'state값 변경'이 일어남->클릭한 목록에 해당되게 Content컴포넌트의 내용이 바뀜
 
+//Number():숫자로 강제로 바꿔주는 js명령
+//data-id속성 사용하지 않고, bind의 두번째 인자 사용해서 할 수도 있음->책 p125
 
-
-
-
+//**상위컴포넌트가 하위컴포넌트로 값을 전달할 때는 'props' 사용 / *하위컴포넌트가 상위컴포넌트의 값을 바꾸고 싶을 때는 '이벤트' 사용
 
--- src/components/TOC.js
+++ src/components/TOC.js
@@ -6,7 +6,16 @@ class TOC extends Component { //'TOC컴포넌트'
         var data = this.props.data; 
         var i=0;
         while(i < data.length) {
-            lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+            lists.push(
+                <li key={data[i].id}>
+                    <a 
+                        href={"/content/"+data[i].id}
+                        data-id={data[i].id} //'data-'id속성 추가
+                        onClick={function(e) {
+                            e.preventDefault();
+                            this.props.onChangePage(e.target.dataset.id); //*onChangePage이벤트의 function의 인자로, 클릭한 목록의 id값을 전달!
+                        }.bind(this)}>{data[i].title}</a>
+                </li>); //<a>태그에, 목록을 클릭했을 때
             i=i+1;
         } 
 
@@ -24,3 +33,8 @@ class TOC extends Component { //'TOC컴포넌트'
 }
 
 export default TOC;
+
+
+//*html에서, data-속성명='속성값'으로 속성값 저장하기 / js에서, dataset.속성명으로 속성값 가져오기
+//*e.target->이벤트가 발생한 태그
+//e.target.'dataset.id' -> <a>태그에 지정해둔 data-id에 접근
